bce638fc74d0183fb1e0a3c36a4dfd92fe26d178
Charith Soysa <charith.14@cse.mrt.ac.lk>
2018-09-23 23:46:15 +0530

    Linked list completed

---
+++ LinkedList_main.c
@@ -0,0 +1,11 @@
+#include "LinkedList.h"
+// #include "RandomNumberGenerator.c"
+#include <stdio.h>
+#include <time.h>
+
+int main (){
+    srand(time(NULL);
+    for(int i = 0; i < 10; i++){
+        printf(" %d ", rand());
+    }
+}
